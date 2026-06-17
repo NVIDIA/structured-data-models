6d96e9836a14a90ab31ab3f1b8424d1656afd7c6
rusty1s <matthias.fey@tu-dortmund.de>
2026-06-17 13:19:10 +0000

    update

--- schemafm/stype.py
+++ schemafm/stype.py
@@ -1,6 +1,10 @@
 from enum import Enum
+from typing import Literal, TypeAlias
 
 
 class Stype(str, Enum):
     numerical = "numerical"
     categorical = "categorical"
+
+
+StypeLike: TypeAlias = Stype | Literal["numerical", "categorical"]
