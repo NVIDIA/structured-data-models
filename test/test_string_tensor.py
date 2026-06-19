7ca2b1b8aefe006d8ead74859d68044c85611a68
rusty1s <matthias.fey@tu-dortmund.de>
2026-06-19 07:55:07 +0000

    update

--- test/test_string_tensor.py
+++ test/test_string_tensor.py
@@ -291,28 +291,34 @@ def test_transpose_permute() -> None:
     assert out.size() == (3, 2)
     assert out.stride() == (4, 1)
     assert out.storage_offset() == 2
+    assert out._data.data_ptr() == tensor._data.data_ptr()
+    assert out._offset.data_ptr() == tensor._offset.data_ptr()
 
-    tensor_3d = StringTensor(
+    tensor = StringTensor(
         data=torch.arange(24, dtype=torch.uint8),
         offset=torch.arange(25),
         size=(2, 3, 4),
         stride=(12, 4, 1),
     )
 
-    out = tensor_3d.transpose(0, -1)
+    out = tensor.transpose(0, -1)
     assert isinstance(out, StringTensor)
     assert out.size() == (4, 3, 2)
     assert out.stride() == (1, 4, 12)
     assert out.storage_offset() == 0
+    assert out._data.data_ptr() == tensor._data.data_ptr()
+    assert out._offset.data_ptr() == tensor._offset.data_ptr()
 
-    out = tensor_3d.permute(2, 0, 1)
+    out = tensor.permute(2, 0, 1)
     assert isinstance(out, StringTensor)
     assert out.size() == (4, 2, 3)
     assert out.stride() == (1, 12, 4)
     assert out.storage_offset() == 0
+    assert out._data.data_ptr() == tensor._data.data_ptr()
+    assert out._offset.data_ptr() == tensor._offset.data_ptr()
 
     with pytest.raises(RuntimeError, match="t\\(\\) expects"):
-        tensor_3d.t()
+        tensor.t()
 
 
 def test_select_slice_narrow_expand() -> None:
@@ -322,7 +328,7 @@ def test_select_slice_narrow_expand() -> None:
         size=(2, 3, 4),
     )
 
-    out = tensor.select(1, 1)
+    out = tensor.select(dim=1, index=1)
     assert isinstance(out, StringTensor)
     assert out.size() == (2, 4)
     assert out.stride() == (12, 1)
@@ -335,12 +341,16 @@ def test_select_slice_narrow_expand() -> None:
     assert out.size() == (2, 2, 2)
     assert out.stride() == (12, 4, 2)
     assert out.storage_offset() == 4
+    assert out._data.data_ptr() == tensor._data.data_ptr()
+    assert out._offset.data_ptr() == tensor._offset.data_ptr()
 
-    out = tensor.narrow(1, 1, 2)
+    out = tensor.narrow(dim=1, start=1, length=2)
     assert isinstance(out, StringTensor)
     assert out.size() == (2, 2, 4)
     assert out.stride() == (12, 4, 1)
     assert out.storage_offset() == 4
+    assert out._data.data_ptr() == tensor._data.data_ptr()
+    assert out._offset.data_ptr() == tensor._offset.data_ptr()
 
     tensor = StringTensor(
         data=torch.arange(4, dtype=torch.uint8),
@@ -354,3 +364,4 @@ def test_select_slice_narrow_expand() -> None:
     assert out.storage_offset() == 0
     assert out._data.data_ptr() == tensor._data.data_ptr()
     assert out._offset.data_ptr() == tensor._offset.data_ptr()
+    out = out.clone()
