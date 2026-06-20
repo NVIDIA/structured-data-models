e355f01cc88908ac3df93afc3a814418147e9866
Matthias Fey <matthias.fey@tu-dortmund.de>
2026-06-20 23:33:41 +0200

    Move `Tensor` implementations to `tensor/` (#37)

--- schemafm/__init__.py
+++ schemafm/__init__.py
@@ -1,9 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 from schemafm.stype import Stype, StypeLike
-from schemafm.varlen_tensor import VarLenTensor
-from schemafm.string_tensor import StringTensor
-from schemafm.table_tensor import TableTensor
+from schemafm.tensor import VarLenTensor, StringTensor, TableTensor
 
 try:
     __version__ = version("schema-fm")
--- schemafm/tensor/string_tensor.py
+++ schemafm/tensor/string_tensor.py
@@ -5,7 +5,7 @@ from typing import Any, ClassVar, cast
 import pyarrow as pa
 import torch
 
-from schemafm import VarLenTensor
+from schemafm.tensor import VarLenTensor
 
 
 class StringTensor(VarLenTensor):
