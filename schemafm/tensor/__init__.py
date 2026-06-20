3458c623493c6a51d0947343a5085e8739aa8ce2
rusty1s <matthias.fey@tu-dortmund.de>
2026-06-20 21:23:32 +0000

    Move tensor modules into tensor package

---
+++ schemafm/tensor/__init__.py
@@ -0,0 +1,9 @@
+from schemafm.tensor.varlen_tensor import VarLenTensor
+from schemafm.tensor.string_tensor import StringTensor
+from schemafm.tensor.table_tensor import TableTensor
+
+__all__ = [
+    "StringTensor",
+    "TableTensor",
+    "VarLenTensor",
+]
