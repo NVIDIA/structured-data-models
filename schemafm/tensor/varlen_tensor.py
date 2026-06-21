7abb5ab2e6cc648c6076bc08690cec2c58f872b9
rusty1s <matthias.fey@tu-dortmund.de>
2026-06-21 18:18:50 +0000

    update

--- schemafm/tensor/varlen_tensor.py
+++ schemafm/tensor/varlen_tensor.py
@@ -235,13 +235,13 @@ class VarLenTensor(Tensor):
     def to_arrow(self) -> pa.Array:
         if self.device.type != "cpu":
             raise TypeError(
-                f"can't convert {self.device} device type tensor to arrow. "
+                f"Can't convert {self.device} device type tensor to arrow. "
                 f"Use Tensor.cpu() to copy the tensor to host memory first."
             )
         if self.requires_grad:
             raise RuntimeError(
-                "Can't call to_arrow() on Tensor that requires grad. "
-                "Use Tensor.detach().to_arrow() instead."
+                "Can't call 'to_arrow()' on Tensor that requires grad. "
+                "Use 'Tensor.detach().to_arrow()' instead."
             )
 
         data, offset = cast(VarLenTensor, self.contiguous()).data_offset
