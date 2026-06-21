66833aa2deadf89b82cea60fc9a71012cbba5f38
rusty1s <matthias.fey@tu-dortmund.de>
2026-06-21 09:05:42 +0000

    update

--- schemafm/tensor/__init__.py
+++ schemafm/tensor/__init__.py
@@ -1,9 +1,7 @@
 from schemafm.tensor.varlen_tensor import VarLenTensor
 from schemafm.tensor.string_tensor import StringTensor
-from schemafm.tensor.table_tensor import TableTensor
 
 __all__ = [
     "StringTensor",
-    "TableTensor",
     "VarLenTensor",
 ]
