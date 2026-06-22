7b12c63b52c58da483f5040b526ac9a26f3b81bb
Matthias Fey <matthias.fey@tu-dortmund.de>
2026-06-22 11:39:15 +0200

    Add `to_copy` implementation (#47)

--- test/tensor/test_categorical.py
+++ test/tensor/test_categorical.py
@@ -2,6 +2,47 @@ import torch
 from schemafm import CategoricalTensor
 
 
+def test_to_copy() -> None:
+    data = torch.tensor([[0, -1, 2], [2, 1, 0]])
+    categories = tuple(torch.arange(3) for _ in range(data.size(-1)))
+    tensor = CategoricalTensor(data, categories)
+
+    out = tensor.to(torch.int32)
+    assert isinstance(out, CategoricalTensor)
+    assert out.dtype == torch.int32
+    assert out.as_tensor().dtype == torch.int32
+    for out_category, category in zip(out.categories, tensor.categories):
+        assert out_category.equal(category)
+        assert out_category.data_ptr() != category.data_ptr()
+
+    out = tensor.to(torch.float32)
+    assert not isinstance(out, CategoricalTensor)
+    assert out.dtype == torch.float32
+
+
+def test_pin_memory() -> None:
+    data = torch.tensor([[0, -1, 2], [2, 1, 0]])
+    categories = tuple(torch.arange(3) for _ in range(data.size(-1)))
+    tensor = CategoricalTensor(data, categories)
+
+    assert not tensor.is_pinned()
+    if torch.cuda.is_available():
+        assert tensor.pin_memory().is_pinned()
+
+
+def test_share_memory() -> None:
+    data = torch.tensor([[0, -1, 2], [2, 1, 0]])
+    categories = tuple(torch.arange(3) for _ in range(data.size(-1)))
+    tensor = CategoricalTensor(data, categories)
+
+    assert not tensor.is_shared()
+    try:
+        tensor.share_memory_()
+        assert tensor.is_shared()
+    except RuntimeError:
+        pass
+
+
 def test_isnan() -> None:
     data = torch.tensor([[0, -1, 2], [-2, 1, 0]])
     categories = tuple(torch.arange(3) for _ in range(data.size(-1)))
