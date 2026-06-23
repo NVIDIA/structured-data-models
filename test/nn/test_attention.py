d1354673b1845b5ab06b813787020fb49c130408
Akihiro Nitta <nitta@akihironitta.com>
2026-06-23 08:26:27 -0700

    Update test/nn/test_attention.py

--- test/nn/test_attention.py
+++ test/nn/test_attention.py
@@ -24,10 +24,7 @@ def test_qassmax(key_len_fn: Callable[[], Tensor | int]) -> None:
     num_heads = 3
     module = QASSMax(channels=channels, num_heads=num_heads, hidden_channels=4)
 
-    query = torch.randn(
-        2 * 3 * num_heads * channels,
-        dtype=torch.float32,
-    ).reshape(2, 3, num_heads, channels)
+    query = torch.randn(2, 3, num_heads, channels)
 
     out = module(query, key_len_fn())
     assert out.shape == query.shape
