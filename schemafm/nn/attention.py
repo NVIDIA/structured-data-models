ca9fea173d370f06822b016deeb20ccdf5a9069b
Akihiro Nitta <nitta@akihironitta.com>
2026-06-24 10:18:14 -0700

    Update schemafm/nn/attention.py

--- schemafm/nn/attention.py
+++ schemafm/nn/attention.py
@@ -10,7 +10,7 @@ from torch import Tensor
 from torch.nn import GELU, Linear, Sequential
 
 if TYPE_CHECKING:
-    from schemafm.nn.rope import RotaryEmbedding
+    from schemafm.nn import RotaryEmbedding
 
 
 class QASSMax(torch.nn.Module):
