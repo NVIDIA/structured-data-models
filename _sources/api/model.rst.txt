f6ef346e75777cdb18893a1a21ae997f2eee5e99
akihironitta <akihironitta@users.noreply.github.com>
2026-06-26 22:22:00 +0000

    deploy: 4c3528677ed1723aa897f4a685f9e39f9670cf67

---
+++ _sources/api/model.rst.txt
@@ -0,0 +1,10 @@
+sdm.model
+=========
+
+.. autosummary::
+   :toctree: generated
+   :nosignatures:
+
+{% for name in api_names("sdm.model") %}
+   sdm.model.{{ name }}
+{% endfor %}
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0.dev1+g0f956c189',
+    VERSION: '0.1.0.dev1+g4c3528677',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
