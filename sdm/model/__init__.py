4c3528677ed1723aa897f4a685f9e39f9670cf67
Akihiro Nitta <nitta@akihironitta.com>
2026-06-26 15:21:23 -0700

    Add `model.TabICLv2` (#89)

--- sdm/model/__init__.py
+++ sdm/model/__init__.py
@@ -1,1 +1,8 @@
 """Structured Data Models."""
+
+from sdm.model.tabiclv2 import TabICLv2
+
+
+__all__ = [
+    "TabICLv2",
+]
--- sdm/model/tabiclv2/__init__.py
+++ sdm/model/tabiclv2/__init__.py
@@ -1,1 +1,8 @@
 """TabICLv2."""
+
+from sdm.model.tabiclv2.model import TabICLv2
+
+
+__all__ = [
+    "TabICLv2",
+]
