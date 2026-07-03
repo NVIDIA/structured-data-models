7f3f0d487363fb85adf5f159e56a776e9eca2b68
rusty1s <matthias.fey@tu-dortmund.de>
2026-07-03 10:36:28 +0000

    update

--- sdm/tensor/__init__.py
+++ sdm/tensor/__init__.py
@@ -2,7 +2,6 @@
 
 from sdm.tensor.var_len import VarLenTensor
 from sdm.tensor.string import StringTensor
-from sdm.tensor.columnar import ColumnarTensor
 from sdm.tensor.categorical import CategoricalTensor
 from sdm.tensor.columnar import ColumnarTensor
 from sdm.tensor.table import TableTensor
