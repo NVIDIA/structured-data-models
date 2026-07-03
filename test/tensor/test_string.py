ca1aded1b413a040ea78eb242ddaf30bba90038b
rusty1s <matthias.fey@tu-dortmund.de>
2026-07-03 18:39:47 +0000

    update

--- test/tensor/test_string.py
+++ test/tensor/test_string.py
@@ -48,6 +48,17 @@ def test_arrow() -> None:
     assert array.type == pa.large_string()
     assert array.to_pylist() == ["hi", "é", "", "abc"]
 
+    tensor = StringTensor.from_list(["hi", "é", "", "abc"])
+    array = tensor[1:].to_arrow()
+    assert array.offset == 1
+    assert array.to_pylist() == ["é", "", "abc"]
+    assert array.buffers()[1].address == tensor._offset.numpy().__array_interface__[
+        "data"
+    ][0]
+    assert array.buffers()[2].address == tensor._data.numpy().__array_interface__[
+        "data"
+    ][0]
+
 
 def test_allowed_dtype() -> None:
     tensor = StringTensor.from_list("hi")
