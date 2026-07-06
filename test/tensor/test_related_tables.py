50066102f170fa43ed966ed6f5997964510cda6c
rusty1s <matthias.fey@tu-dortmund.de>
2026-07-06 07:56:54 +0000

    update

--- test/tensor/test_related_tables.py
+++ test/tensor/test_related_tables.py
@@ -98,6 +98,7 @@ def data() -> tuple[TableTensor, RelatedTables]:
 def test_edge_indices(data: tuple[TableTensor, RelatedTables]) -> None:
     task_table, related_tables = data
     edge_indices = related_tables.edge_indices(task_table)
+    edge_index = related_tables.edge_index(task_table)
 
     assert len(edge_indices) == 3
     assert edge_indices[0].equal(torch.tensor([[0, 1, 2, 3], [0, 1, 2, 3]]))
@@ -106,6 +107,14 @@ def test_edge_indices(data: tuple[TableTensor, RelatedTables]) -> None:
     )
     assert edge_indices[2].equal(
         torch.tensor([[0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 3]])
+    )
+    assert edge_index.equal(
+        torch.tensor(
+            [
+                [0, 1, 2, 3, 8, 9, 10, 11, 12, 13, 8, 9, 10, 11, 12, 13],
+                [4, 5, 6, 7, 4, 4, 5, 7, 7, 7, 14, 15, 16, 17, 18, 17],
+            ]
+        )
     )
 
 
