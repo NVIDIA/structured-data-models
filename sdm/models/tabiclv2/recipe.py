fe33c051d455098e92a5b77030454fe1e22b4735
Ramona Bendias <rbendias@nvidia.com>
2026-07-10 01:46:14 +0200

    Update categorical transformations in recipe.py

--- sdm/models/tabiclv2/recipe.py
+++ sdm/models/tabiclv2/recipe.py
@@ -29,7 +29,7 @@ def default_recipe() -> Recipe:
         features=[
             StypeDispatch(
                 numerical=Identity(),
-                categorical=[CategoricalImpute(), ToNumerical()],
+                categorical=[CategoricalAlign(), CategoricalImpute(), ToNumerical()],
             ),
             MeanImpute(),
             ConstantFilter(),
