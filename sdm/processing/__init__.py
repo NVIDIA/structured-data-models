c04f575a63a059b42e3c284199566b203fd194b7
Ramona Bendias <rbendias@nvidia.com>
2026-07-11 02:53:10 +0200

    Add task-aware output dispatch (#220)
    Co-authored-by: Akihiro Nitta <nitta@akihironitta.com>

--- sdm/processing/__init__.py
+++ sdm/processing/__init__.py
@@ -4,6 +4,7 @@ from sdm.processing.base import Processor, InvertibleMixin
 from sdm.processing.sequential import Sequential
 from sdm.processing.choice import Choice
 from sdm.processing.stype_dispatch import StypeDispatch
+from sdm.processing.task_dispatch import TaskDispatch
 from sdm.processing.clip import Clip
 from sdm.processing.class_shuffle import ClassShuffle
 from sdm.processing.constant_filter import ConstantFilter
@@ -24,6 +25,7 @@ __all__ = [
     "Sequential",
     "Choice",
     "StypeDispatch",
+    "TaskDispatch",
     "Clip",
     "ClassShuffle",
     "ConstantFilter",
