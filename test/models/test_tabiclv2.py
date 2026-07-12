99422a4360f5c9f3bf51c24434c2ba5541c0783f
Rishi Puri <riship@nvidia.com>
2026-07-11 20:17:22 -0700

    Update test/models/test_tabiclv2.py
    Co-authored-by: Akihiro Nitta <nitta@akihironitta.com>

--- test/models/test_tabiclv2.py
+++ test/models/test_tabiclv2.py
@@ -257,6 +257,7 @@ def test_tabiclv2_fit_predict_compile(device: torch.device) -> None:
 
 
 @onlyFullTest
+@onlyFullTest
 @withCUDA
 def test_tabiclv2_autocast_compile(device: torch.device) -> None:
     torch.manual_seed(0)
