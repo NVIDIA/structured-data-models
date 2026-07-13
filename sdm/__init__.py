4e52ff5697eda0ad306bf9b6496671cf1f1cbe08
rusty1s <matthias.fey@tu-dortmund.de>
2026-07-13 10:05:25 +0000

    update

--- sdm/__init__.py
+++ sdm/__init__.py
@@ -2,7 +2,6 @@
 
 from importlib.metadata import PackageNotFoundError, version
 
-
 from sdm.stype import Stype, StypeLike, infer_stypes
 from sdm.tensor import (
     VarLenTensor,
@@ -22,7 +21,6 @@ try:
     __version__ = version("structured-data-models")
 except PackageNotFoundError:
     __version__ = "0+unknown"
-
 
 __all__ = [
     "Stype",
