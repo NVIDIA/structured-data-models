7fff01c1b62a2f368321ace4e1ab8b928e543159
Ramona Bendias <rbendias@nvidia.com>
2026-07-13 17:38:28 +0200

    Fix typo in Model class docstring (#268)

--- sdm/tensor/categorical.py
+++ sdm/tensor/categorical.py
@@ -142,7 +142,7 @@ class CategoricalTensor(Tensor):
             >>> tensor([[ 0],
             >>>         [-1],
             >>>         [ 1]])
-            print(tensor.categories[0].to_list())
+            print(tensor.categories[0].tolist())
             >>> ['foo', 'bar']
 
         Args:
