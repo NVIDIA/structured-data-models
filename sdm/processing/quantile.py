8c91abc6cefab830e9f18f61f295d633f12127bd
RBendias <rbendias@nvidia.com>
2026-07-17 11:56:54 +0000

    Remove NaN-aware numerical processor paths

--- sdm/processing/quantile.py
+++ sdm/processing/quantile.py
@@ -111,7 +111,7 @@ class Quantile(Processor, InvertibleMixin):
         else:
             input_sample = numerical
 
-        self.quantiles = torch.nanquantile(
+        self.quantiles = torch.quantile(
             input_sample,
             self.references,
             dim=0,
