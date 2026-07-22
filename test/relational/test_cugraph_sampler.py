bf585c6c1badf49dd2f344427865c1d974865fd1
Lei Sun <autumnust@users.noreply.github.com>
2026-07-22 00:02:52 +0000

    Optimize temporal fanout by reachable edge type

--- test/relational/test_cugraph_sampler.py
+++ test/relational/test_cugraph_sampler.py
@@ -398,7 +398,7 @@ def test_cugraph_temporal_sampler_uses_bounded_uniform_fanout(
         task_time_column="cutoff",
     )
 
-    assert fanouts == [[1, 1, 1, 1], [2, 2, 2, 2]]
+    assert fanouts == [[1, 0, 0, 0], [0, 2, 2, 0]]
 
 
 @onlyCUDA
