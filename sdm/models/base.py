6f381523b210cd088dccfe253e52f3e6aa0d97a4
RBendias <rbendias@nvidia.com>
2026-07-22 07:39:47 +0000

    Use TableTensor dtype casts for model outputs

--- sdm/models/base.py
+++ sdm/models/base.py
@@ -179,10 +179,7 @@ class ICLModel(torch.nn.Module, ABC):
             outs.append(out)
 
         out: TableTensor = cast(TableTensor, torch.stack(outs, dim=0))
-        if out.numerical.dtype != torch.float32:
-            out = out.replace_blocks(
-                numerical=out.numerical.to(torch.float32),
-            )
+        out = cast(TableTensor, out.to(x_query_i.dtype))
         return recipe.output.transform(out)
 
     @_maybe_inference_mode()
@@ -375,10 +372,7 @@ class ICLModel(torch.nn.Module, ABC):
             outs.append(out)
 
         out: TableTensor = cast(TableTensor, torch.stack(outs, dim=0))
-        if out.numerical.dtype != torch.float32:
-            out = out.replace_blocks(
-                numerical=out.numerical.to(torch.float32),
-            )
+        out = cast(TableTensor, out.to(x_i.dtype))
         return recipe.output.transform(out)
 
     def __repr__(self) -> str:
