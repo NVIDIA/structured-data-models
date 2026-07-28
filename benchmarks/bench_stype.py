0120e4f665fa03ab03f3c18bf013f7e947668aa8
JGagacheva <j.gagaceva@gmail.com>
2026-07-28 18:38:31 +0000

    remove benchmark script from tracking

--- benchmarks/bench_stype.py
+++ benchmarks/bench_stype.py
@@ -30,7 +30,11 @@ import pyarrow as pa
 import torch
 from sdm import infer_stypes
 
-BACKENDS = ["arrow", "pandas", "cudf"]
+BACKENDS = [
+    "arrow",
+    "pandas",
+    "cudf",
+]
 ROW_COUNTS = [100, 1_000, 5_000]
 COLUMN_COUNTS = [1, 10]
 REPEATS = 5
