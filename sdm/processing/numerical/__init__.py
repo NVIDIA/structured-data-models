2b1514bb03fb2f4ffb6f25aab7e92ed846d5757a
Akihiro Nitta <nitta@akihironitta.com>
2026-07-31 21:58:20 -0700

    Generalise `SliceColumns` to support any stypes (#476)
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- sdm/processing/numerical/__init__.py
+++ sdm/processing/numerical/__init__.py
@@ -8,7 +8,6 @@ from sdm.processing.numerical.power import PowerTransform
 from sdm.processing.numerical.quantile import QuantileTransform
 from sdm.processing.numerical.standardize import Standardize
 from sdm.processing.numerical.constant import DropConstantColumns
-from sdm.processing.numerical.slice_columns import SliceColumns
 
 __all__ = [
     "Clip",
@@ -19,5 +18,4 @@ __all__ = [
     "QuantileTransform",
     "Standardize",
     "DropConstantColumns",
-    "SliceColumns",
 ]
