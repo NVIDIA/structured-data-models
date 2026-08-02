6daf670cd3cd8666fb777f0b398924f3ebb7d2d2
Akihiro Nitta <nitta@akihironitta.com>
2026-08-02 06:21:21 +0000

    update

--- sdm/processing/numerical/pca.py
+++ sdm/processing/numerical/pca.py
@@ -32,7 +32,6 @@ class PCA(Processor):
         self.num_components = num_components
         self.register_buffer("mean", torch.empty(0), persistent=True)
         self.register_buffer("components", torch.empty(0), persistent=True)
-        self._output_columns: tuple[str, ...] = ()
 
     def _fit(
         self,
