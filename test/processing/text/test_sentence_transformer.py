eec1702842052df673b96937d5a92adf556e3516
rusty1s <matthias.fey@tu-dortmund.de>
2026-08-09 18:15:40 +0000

    update

--- test/processing/text/test_sentence_transformer.py
+++ test/processing/text/test_sentence_transformer.py
@@ -9,43 +9,8 @@ from sdm.testing import withCUDA
 
 
 @withCUDA
-def test_forward(
-    device: torch.device,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    sentence_transformers = pytest.importorskip("sentence_transformers")
-
-    class _SentenceTransformer:
-        values: list[str]
-
-        def __init__(self, model_name: str) -> None:
-            assert model_name == "dummy"
-
-        def get_embedding_dimension(self) -> int:
-            return 4
-
-        def encode(
-            self,
-            values: list[str],
-            *,
-            show_progress_bar: bool,
-            convert_to_tensor: bool,
-            device: str,
-            batch_size: int,
-        ) -> torch.Tensor:
-            assert not show_progress_bar
-            assert convert_to_tensor
-            assert batch_size == 16
-            data = [{"": 0, "a": 1, "b": 2, "c": 3}[value] for value in values]
-            return (
-                torch.tensor(data, device=device).unsqueeze(-1).expand(-1, 4)
-            )
-
-    monkeypatch.setattr(
-        sentence_transformers,
-        "SentenceTransformer",
-        _SentenceTransformer,
-    )
+def test_forward(device: torch.device) -> None:
+    pytest.importorskip("sentence_transformers")
 
     table = TableTensor(
         columns={"text": ("title", "body")},
@@ -58,21 +23,12 @@ def test_forward(
         ),
     )
 
-    processor = SentenceTransformer("dummy", batch_size=16).to(device)
-    output = processor(table)
+    output = SentenceTransformer(
+        "sentence-transformers-testing/stsb-bert-tiny-safetensors"
+    ).to(device)(table)
 
     assert output.columns[Stype.numerical] == tuple(
-        f"{column}__emb{i}" for column in ("title", "body") for i in range(4)
+        f"{column}__emb{i}" for column in ("title", "body") for i in range(128)
     )
-    assert output.numerical.shape == (2, 8)
+    assert output.numerical.shape == (2, 256)
     assert output.numerical.device == device
-    assert output.numerical.equal(
-        torch.tensor(
-            [
-                [1, 1, 1, 1, 2, 2, 2, 2],
-                [3, 3, 3, 3, 0, 0, 0, 0],
-            ],
-            dtype=output.numerical.dtype,
-            device=device,
-        )
-    )
