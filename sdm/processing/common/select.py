227621e65ba58966d13963a6564a326f32de45a3
Ramona Bendias <rbendias@nvidia.com>
2026-08-13 09:59:11 +0200

    Update sdm/processing/common/select.py
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- sdm/processing/common/select.py
+++ sdm/processing/common/select.py
@@ -12,8 +12,7 @@ class SelectColumns(EnsembleProcessor):
     r"""Select a subset of columns for each semantic type.
 
     Args:
-        max_columns: The maximum number of columns to keep for each semantic
-            type in each ensemble member. Must be non-negative.
+        max_columns: The maximum number of columns to keep per semantic type.
         method: The column selection method.
             ``"first"`` keeps the first columns according to their order within
             each semantic block. ``"round_robin"`` assigns each ensemble
