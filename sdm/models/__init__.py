65e0b0718c0ddaad1ff43632f6dae491ae603bd9
Ard <88337265+aw471@users.noreply.github.com>
2026-08-19 13:31:33 -0700

    [Kumo Tabular 6/n] Add model wrapper

--- sdm/models/__init__.py
+++ sdm/models/__init__.py
@@ -3,7 +3,7 @@
 from sdm.models.base import ICLModel
 from sdm.models.tabiclv2 import TabICLv2
 from sdm.models.tabfm import TabFM
-from sdm.models.kumo import KumoRelational
+from sdm.models.kumo import KumoRelational, KumoTabular
 
 
 __all__ = [
@@ -11,4 +11,5 @@ __all__ = [
     "TabICLv2",
     "TabFM",
     "KumoRelational",
+    "KumoTabular",
 ]
