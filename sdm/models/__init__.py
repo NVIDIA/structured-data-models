419690326d4e7b43cd4a78428cc40424060dfa45
Matthias Fey <matthias.fey@tu-dortmund.de>
2026-08-31 14:20:22 +0200

    Drop `KumoTabular` from the documentation (#744)

--- sdm/models/__init__.py
+++ sdm/models/__init__.py
@@ -3,7 +3,7 @@
 from sdm.models.base import ICLModel
 from sdm.models.tabiclv2 import TabICLv2
 from sdm.models.tabfm import TabFM
-from sdm.models.kumo import KumoRelational, KumoTabular
+from sdm.models.kumo import KumoRelational
 
 
 __all__ = [
@@ -11,5 +11,4 @@ __all__ = [
     "TabICLv2",
     "TabFM",
     "KumoRelational",
-    "KumoTabular",
 ]
