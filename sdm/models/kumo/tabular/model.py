fd88e4cd8c72ee6c48b0cf6a57e5681a3deaede2
rusty1s <matthias.fey@tu-dortmund.de>
2026-09-01 09:21:56 +0000

    remap_ckpt

--- sdm/models/kumo/tabular/model.py
+++ sdm/models/kumo/tabular/model.py
@@ -104,6 +104,7 @@ class KumoTabular(ICLModel):  # noqa: D101
                 revision="v1.0.1",
             )
             ckpt = torch.load(path, map_location=device, weights_only=True)
+            ckpt = remap_ckpt(ckpt, is_classifier=task == Task.classification)
             model.load_state_dict(ckpt, assign=True)
 
         return model
@@ -266,3 +267,41 @@ class _KumoTabular(torch.nn.Module):
         x = self.row_embedding(x, y, cache=cache)
         x = self.row_project(x)
         return self.icl_block(x=x, y=y, cache=cache)
+
+
+def remap_ckpt(  # noqa: D103
+    ckpt: dict[str, Tensor],
+    is_classifier: bool,
+) -> dict[str, Tensor]:
+
+    out: dict[str, Tensor] = {}
+    for key, value in ckpt.items():
+        if key == "y_encoder.weight":
+            if is_classifier:
+                out["row_embedding.y_emb.weight"] = (
+                    value.T + ckpt["y_encoder.bias"]
+                )
+            else:
+                out["row_embedding.y_lin.weight"] = value
+            continue
+
+        if is_classifier and key == "y_encoder.bias":
+            continue
+
+        if key == "table_encoder.cls_tokens":
+            out["row_embedding.readout_token"] = value
+            continue
+
+        if key.startswith("table_encoder."):
+            key = key.removeprefix("table_encoder.")
+            out[f"row_embedding.{key}"] = value
+            continue
+
+        if key.startswith("cls_downproject."):
+            key = key.removeprefix("cls_downproject.")
+            out[f"row_project.{key}"] = value
+            continue
+
+        out[key] = value
+
+    return out
