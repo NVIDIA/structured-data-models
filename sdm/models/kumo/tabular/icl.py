a58f4f13e46c40e9d83f687a78be8e25e65f4a7e
rusty1s <matthias.fey@tu-dortmund.de>
2026-09-09 11:24:39 +0000

    update

--- sdm/models/kumo/tabular/icl.py
+++ sdm/models/kumo/tabular/icl.py
@@ -100,7 +100,7 @@ class ICLBlock(torch.nn.Module):
                 del result
                 continue
 
-            key, value = layer(
+            x_context, (key, value) = layer(
                 query=x[..., :0, :] if last_layer else x[..., :R_train, :],
                 key_value=x[..., :R_train, :],
                 return_key_value=True,
