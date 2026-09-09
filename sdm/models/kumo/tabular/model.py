0ecf34fa7eaa8e1ad54d4954c195e83d34963b88
Matthias Fey <matthias.fey@tu-dortmund.de>
2026-09-09 13:39:27 +0200

    Move `remap_ckpt` (#817)

--- sdm/models/kumo/tabular/model.py
+++ sdm/models/kumo/tabular/model.py
@@ -11,6 +11,7 @@ from sdm import Recipe, RelatedTables, Stype, TableTensor, Task, TaskLike
 from sdm.cache import Cache
 from sdm.models import ICLModel
 from sdm.models._huggingface import download_checkpoint
+from sdm.models.kumo.tabular.ckpt import remap_ckpt
 from sdm.models.kumo.tabular.icl import ICLBlock
 from sdm.models.kumo.tabular.recipe import default_recipe
 from sdm.models.kumo.tabular.row_embedding import RowEmbedding
@@ -261,45 +262,3 @@ class _KumoTabular(torch.nn.Module):
         x = self.row_embedding(x, y, categorical_mask, cache=cache)
         x = self.row_project(x)
         return self.icl_block(x=x, y=y, cache=cache)
-
-
-def remap_ckpt(  # noqa: D103
-    ckpt: dict[str, Tensor],
-    is_classifier: bool,
-) -> dict[str, Tensor]:
-
-    out: dict[str, Tensor] = {}
-    for key, value in ckpt.items():
-        if key == "y_encoder.weight":
-            if is_classifier:
-                out["row_embedding.y_emb.weight"] = (
-                    value.T + ckpt["y_encoder.bias"]
-                )
-            else:
-                out["row_embedding.y_lin.weight"] = value
-            continue
-
-        if is_classifier and key == "y_encoder.bias":
-            continue
-
-        if key == "table_encoder.cls_tokens":
-            out["row_embedding.readout_token"] = value
-            continue
-
-        if key.startswith("table_encoder."):
-            key = key.removeprefix("table_encoder.")
-            out[f"row_embedding.{key}"] = value
-            continue
-
-        if key.startswith("cls_downproject."):
-            key = key.removeprefix("cls_downproject.")
-            out[f"row_project.{key}"] = value
-            continue
-
-        if key.startswith("cell_embedding."):
-            out[f"row_embedding.{key}"] = value
-            continue
-
-        out[key] = value
-
-    return out
