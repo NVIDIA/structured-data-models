7a350f2fb101f6540afcbaaa09e93a6bf0595b21
Cedric Lorenz <clorenz@nvidia.com>
2026-09-11 08:26:17 -0700

    Preserve Latin column sampling

--- sdm/models/kumo/tabular/recipe.py
+++ sdm/models/kumo/tabular/recipe.py
@@ -12,9 +12,10 @@ def default_recipe(max_columns: int | None = None) -> sp.Recipe:  # noqa: D103
             ),
             sp.StypeDispatch(
                 numerical=[
+                    sp.ShuffleColumns(method="latin"),
                     sp.SelectColumns(
                         500 if max_columns is None else max_columns,
-                        method="round_robin",
+                        method="first",
                     ),
                     sp.ImputeMean(),
                     sp.DropConstantColumns(),
@@ -27,7 +28,6 @@ def default_recipe(max_columns: int | None = None) -> sp.Recipe:  # noqa: D103
                     ),
                     sp.ClipSigma(threshold=4.0),
                     sp.FlipSign(),
-                    sp.ShuffleColumns(method="latin"),
                 ],
             ),
         ],
