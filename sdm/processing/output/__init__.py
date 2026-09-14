4ca8aee6965e88d8715896be823a2e5d46629f86
Matthias Fey <matthias.fey@tu-dortmund.de>
2026-09-14 09:08:59 +0200

    Add `SortQuantiles` processor (#853)

--- sdm/processing/output/__init__.py
+++ sdm/processing/output/__init__.py
@@ -2,5 +2,10 @@
 
 from sdm.processing.output.reduce import ReduceEstimators
 from sdm.processing.output.softmax import Softmax
+from sdm.processing.output.sort import SortQuantiles
 
-__all__ = ["ReduceEstimators", "Softmax"]
+__all__ = [
+    "ReduceEstimators",
+    "Softmax",
+    "SortQuantiles",
+]
