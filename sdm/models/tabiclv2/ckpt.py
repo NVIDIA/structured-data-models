01f5a0801a29f61b6b7fd8d3ee53cb526bdcac0b
Akihiro Nitta <nitta@akihironitta.com>
2026-09-14 16:14:53 -0700

    update

--- sdm/models/tabiclv2/ckpt.py
+++ sdm/models/tabiclv2/ckpt.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025, Soda team @ Inria
+# Licensed under the BSD 3-Clause License; see LICENSE.
+
 # SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
