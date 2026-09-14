056c4998ed98d77ef55fb27d8a27c71eac99ecda
Matthias Fey <matthias.fey@tu-dortmund.de>
2026-09-14 12:55:40 +0200

    Move `callbacks/` to `models/` (#859)

--- test/models/test_base.py
+++ test/models/test_base.py
@@ -7,8 +7,8 @@ import torch
 import sdm.processing as sp
 from sdm import ColumnarTensor, RelatedTables, Stype, TableTensor
 from sdm.cache import Cache
-from sdm.callbacks import Callback
 from sdm.models import ICLModel
+from sdm.models.callback import Callback
 from sdm.processing import InvertibleMixin, Processor
 
 
