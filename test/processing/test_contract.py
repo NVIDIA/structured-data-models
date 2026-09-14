d654aa8289356b4cf1a95e4e972622c3de0f7505
rusty1s <matthias.fey@tu-dortmund.de>
2026-09-14 07:00:53 +0000

    update

--- test/processing/test_contract.py
+++ test/processing/test_contract.py
@@ -130,6 +130,7 @@ PROCESSOR_CASES = (
     ProcessorCase(sp.ImputeMode()),
     ProcessorCase(sp.AddCalendarFields(["month"])),
     ProcessorCase(sp.Softmax()),
+    ProcessorCase(sp.SortQuantiles()),
     ProcessorCase(sp.ReduceEstimators(), _make_reduction_table),
     ProcessorCase(sp.EnsembleProcessorAdapter(sp.Standardize())),
     ProcessorCase(
