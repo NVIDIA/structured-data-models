5ea724c4bfcdd24a4c004ccf254d869a4133e8c6
Matthias Fey <matthias.fey@tu-dortmund.de>
2026-09-15 08:25:37 +0200

    Alternative `CellEmbedding` impl (#863)

--- sdm/models/kumo/tabular/model.py
+++ sdm/models/kumo/tabular/model.py
@@ -121,7 +121,7 @@ class KumoTabular(ICLModel):
                 is_classifier=task == Task.classification,
                 num_layers=MODEL_KWARGS[size]["num_embedding_layers"],
             )
-            model.load_state_dict(ckpt, strict=True, assign=True)
+            model.load_state_dict(ckpt, assign=True)
 
         return model
 
