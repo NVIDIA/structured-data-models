dc26df5ddfd0f90a0e41e153f4abf8b36999e7b8
RBendias <rbendias@nvidia.com>
2026-09-15 11:58:06 +0000

    update

--- test/processing/numerical/test_quantile.py
+++ test/processing/numerical/test_quantile.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from typing import cast
+
 import pytest
 import torch
 
