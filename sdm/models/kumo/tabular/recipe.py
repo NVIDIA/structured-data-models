2ae44d7f69431ee24ff2962c03c9fd2ed2f4af85
RBendias <rbendias@nvidia.com>
2026-09-18 08:07:59 +0000

    Update KumoTabular processor recipe
    Signed-off-by: RBendias <rbendias@nvidia.com>

--- sdm/models/kumo/tabular/recipe.py
+++ sdm/models/kumo/tabular/recipe.py
@@ -4,27 +4,61 @@
 import sdm.processing as sp
 
 
+def _normalize() -> list[sp.Processor]:
+    return [
+        sp.Callable(
+            lambda table: table.replace_blocks(
+                numerical=table.numerical.double()
+            )
+        ),
+        sp.Standardize(eps=1e-6),
+        sp.Clip(min_value=-100.0, max_value=100.0),
+        sp.Choice(
+            sp.Identity(),
+            sp.PowerTransform(),
+            [
+                sp.RobustScale(),
+                sp.ClipSoft(3.0),
+            ],
+            method="round_robin",
+        ),
+        sp.ClipSigma(threshold=4.0),
+    ]
+
+
 def default_recipe() -> sp.Recipe:  # noqa: D103
     return sp.Recipe(
         features=[
             sp.StypeDispatch(
                 categorical=[
-                    sp.AlignCategories(sort_by="value", min_frequency=2),
-                    sp.ToNumerical(),
+                    sp.AlignCategories(sort_by="value"),
+                    sp.AddCategoryCounts(),
                 ],
             ),
             sp.StypeDispatch(
                 numerical=[
                     sp.DropConstantColumns(),
-                    sp.Standardize(eps=1e-6),
-                    sp.Clip(min_value=-100.0, max_value=100.0),
-                    sp.Choice(
-                        sp.Identity(),
-                        sp.PowerTransform(),
-                        method="round_robin",
-                    ),
-                    sp.ClipSigma(threshold=4.0),
+                    *_normalize(),
                     sp.FlipSign(),
+                    sp.Callable(
+                        lambda table: table.replace_blocks(
+                            numerical=table.numerical.float()
+                        )
+                    ),
+                ],
+                categorical=[
+                    sp.ToNumerical(),
+                    sp.DropConstantColumns(),
+                    *_normalize(),
+                    sp.Callable(
+                        lambda table: table.replace_blocks(
+                            numerical=table.numerical.float()
+                        )
+                    ),
+                ],
+            ),
+            sp.StypeDispatch(
+                numerical=[
                     sp.ShuffleColumns(method="latin"),
                     sp.SelectColumns(500, method="first"),
                 ],
@@ -42,9 +76,17 @@ def default_recipe() -> sp.Recipe:  # noqa: D103
                 ],
             ),
         ],
-        output=[
-            sp.TaskDispatch(regression=sp.SortQuantiles()),
-            sp.ReduceEstimators(method="mean"),
-            sp.TaskDispatch(classification=sp.Softmax(temperature=1.0)),
-        ],
+        output=sp.TaskDispatch(
+            classification=[
+                sp.ReduceEstimators(method="mean"),
+                sp.Softmax(temperature=1.0),
+            ],
+            regression=[
+                sp.ReduceQuantiles(),
+                sp.ReduceEstimators(
+                    method="trimmed_mean",
+                    proportion=0.2,
+                ),
+            ],
+        ),
     )
