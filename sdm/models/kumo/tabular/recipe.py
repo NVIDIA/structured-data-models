2ce3295919a37aa5e30df5e2798551e9f2a5a1cf
rusty1s <matthias.fey@tu-dortmund.de>
2026-09-21 13:49:14 +0000

    update

--- sdm/models/kumo/tabular/recipe.py
+++ sdm/models/kumo/tabular/recipe.py
@@ -9,7 +9,7 @@ def default_recipe() -> sp.Recipe:  # noqa: D103
         return sp.Sequential(
             sp.DropConstantColumns(),
             sp.Standardize(eps=1e-6),
-            sp.Clip(min_value=-100.0, max_value=100.0),
+            sp.Clip(-100.0, 100.0),
             sp.Choice(
                 sp.Identity(),
                 sp.PowerTransform(),
